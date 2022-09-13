8b37ce03fc27ee1a05847b290812e0fb5ab47d7c
Savcinschi Egor <sasha_lagun1995@mail.ru>
2022-09-13 12:55:28 +0300

    useParams

--- src/App.tsx
+++ src/App.tsx
@@ -1,30 +1,33 @@
 import React from 'react';
 import './App.css';
-import {NavLink, Route, Routes} from "react-router-dom";
+import {NavLink, Route, Routes, useParams} from "react-router-dom";
+
+
+const Profile = () => {
+    const params = useParams<'id'>()
+
+    const some = params.id
+
+    console.log(some)
+
+    return <div>profile</div>
+}
 
 function App() {
-    return (
-        <div className="App">
-            <NavLink to={'/'}>main</NavLink>---
-            <NavLink to={'/login'}>login</NavLink>---
-            <NavLink to={'/profile'}>profile</NavLink>---
-            <NavLink to={'/profile/settings'}>settings</NavLink>
-            <Routes>
-                <Route path={'/*'} element={<div>404</div>}></Route>
-                <Route path={'/'} element={<div>main</div>}></Route>
-                <Route path={'/login'} element={<div>login</div>}></Route>
-                <Route path={'/profile/*'} element={
-                    <div>
-                        profile
-                        <Routes>
-                            <Route path={'/settings'} element={<div>settings</div>}></Route>
-                        </Routes>
-                    </div>
-                }></Route>
-
-            </Routes>
-        </div>
-    );
+  return (
+    <div className="App">
+      <NavLink to={'/'}>main</NavLink>---
+      <NavLink to={'/login'}>login</NavLink>---
+      <NavLink to={'/profile'}>profile</NavLink>---
+      <NavLink to={'/profile/1'}>profile/1</NavLink>
+      <Routes>
+        <Route path={'/*'} element={<div>404</div>}/>
+        <Route path={'/'} element={<div>main</div>}/>
+        <Route path={'/login'} element={<div>login</div>}/>
+        <Route path={'/profile/:id'} element={<Profile/>}/>
+      </Routes>
+    </div>
+  );
 }
 
 export default App;
